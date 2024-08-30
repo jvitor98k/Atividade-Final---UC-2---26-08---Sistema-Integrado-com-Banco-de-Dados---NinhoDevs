e9d8f7120b412609a5fc2aff754836e7a1d3b622
Yan Dias <yandias123.yd@gmail.com>
2024-08-30 14:16:58 -0300

    Atualização de Código 1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 from Conexao import Conexao
+
+conexaoBD = Conexao("localhost", "root", "123456", "escola_romeu")
+
+while True:
+    print(f"{"-"* 15} Sistema de Gestão Escolar {"-"* 15}")
+    print('''
+    1. Ver Disciplinas.
+    2. Ver Professores.
+    0. Encerrar programa.
+    ''')
+    op = int(input("selecione a opção que deseja: "))
+    if (op == 0):
+        print("Programa Encerrado")
+        break
+    elif (op == 1):
+
+
+        disciplinas = conexaoBD.consultar("SELECT * FROM disciplina")
+        for disciplina in disciplinas:
+            print("ID  | Nome da Disciplina ")
+            print(f"{disciplina[0]}   | {disciplina[1]}")
+        # op = int(input("Digite o ID da Disciplina que deseja para vê mais informações: "))
+        # conexaoBD.consultarComParametros("SELECT * FROM ")
+
+
+    elif (op == 2):
+        try:
+            professores = conexaoBD.consultar("SELECT * FROM professor")
+            for professor in professores:
+                print("ID  | Nome do Professor ")
+                print(f"{professor[0]}   | {professor[1]}")
+
+        except:
+            print("Tente uma alternativa correta! ")
+
+
+
+    input("Digite enter para continuar")
